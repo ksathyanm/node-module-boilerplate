2bccbecc6f929a59fae955aa685ac3e1f1ee0b8b
Sathyamoorthi Kandhasamy <10458711+ksathyanm@users.noreply.github.com>
2020-08-25 17:58:58 -0500

    Support Node 10, 12, 14 and add github actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "commonjs": true,
-    "es2020": true,
-  },
   "extends": [
     "eslint:recommended",
     "plugin:node/recommended",
     "google",
   ],
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es2020": true,
+  },
   "parserOptions": {
     "ecmaVersion": 2020,
     "sourceType": "script",
